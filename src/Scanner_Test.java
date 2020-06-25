cc469a648b172ec755e29dfe22fd340e6309c04f
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-25 15:13:52 -0400

    made starter code, Scanner confirms working.

--- src/Scanner_Test.java
+++ src/Scanner_Test.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Scanner_Test {
+
+    public String getUserInput() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("State a number: ");
+        String num1 = input.nextLine();
+        return num1;
+    }
+
+    public static void main(String[] args) {
+        Scanner_Test test = new Scanner_Test();
+        System.out.println("You gave the number " + test.getUserInput());
+    }
 
 }
