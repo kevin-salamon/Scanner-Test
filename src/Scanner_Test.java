cad53a2a0e19b4ccbc0b66aac452a88f66986622
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-25 16:35:09 -0400

    tested calculator with user input successfully

--- src/Scanner_Test.java
+++ src/Scanner_Test.java
@@ -2,16 +2,41 @@ import java.util.Scanner;
 
 public class Scanner_Test {
 
-    public String getUserInput() {
+    public void getUserInput() {
         Scanner input = new Scanner(System.in);
-        System.out.println("State a number: ");
-        String num1 = input.nextLine();
-        return num1;
+        System.out.println("State your first number: ");
+        double num1 = Integer.parseInt(input.nextLine());
+        System.out.println("State your operator: ");
+        String operator = input.nextLine();
+        System.out.println("State your second number: ");
+        double num2 = Integer.parseInt(input.nextLine());
+
+        double result;
+
+        if (operator.equals("+")) {
+            result = num1 + num2;
+        } else if (operator.equals("-")) {
+            result = num1 - num2;
+        } else if (operator.equals("*")) {
+            result = num1 * num2;
+        } else if (operator.equals("/")) {
+            result = num1 / num2;
+        } else {
+            result = -1;
+        }
+
+        System.out.println("The first number you chose is " + num1);
+        System.out.println("The operator you chose is " + operator);
+        System.out.println("The second number you chose is " + num2);
+        System.out.println("Thus, your result is " + result);
+
     }
 
     public static void main(String[] args) {
         Scanner_Test test = new Scanner_Test();
-        System.out.println("You gave the number " + test.getUserInput());
+        System.out.println("This is a simple calculator");
+        System.out.println("Please enter your math below:");
+        test.getUserInput();
     }
 
 }
